5c9b7ab092ddcb7d35300d887e79738d3e09e9d4
tanmoymallik3110 <tanmoymallikch@gmail.com>
2023-06-05 20:33:34 +0600

    first commit

---
+++ lib/screens/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ToDo App"),
+      ),
+      body: Container(
+        child: Text("Home page"),
+      ),
+    );
+  }
+}
